9f8d9c7f53b9801622c76e9b7bcfbb326898f2ce
umamarajpoot <umamarajpoottasswor@gmail.com>
2024-12-09 01:29:47 +0500

    commit

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,8 +2,6 @@ import Link from "next/link";
 import Hero from "./Hero"
 import React from "react"
 import Image from "next/image"
-import google from "/google.png"
-import apple from "/apple.png"
 
 export default function SignUpPage() {
   return (
@@ -57,11 +55,11 @@ export default function SignUpPage() {
           <div className="text-center mt-8">
             <p>or</p>
             <button className="w-full bg-gray-100 border text-black py-2 rounded mt-2 flex items-center justify-center">
-              <Image src={google} alt="Google" className="w-[20px] h-[20px] left-[16px] mr-2" />
+              <Image src="/google" alt="Google" className="w-[20px] h-[20px] left-[16px] mr-2" />
               Sign up with Google
             </button>
             <button className="w-full bg-gray-100 border text-black py-2 rounded mt-2 flex items-center justify-center">
-              <Image src={apple} alt="Apple" className="w-[24px] h-[24px] left-[16px] mr-2" />
+              <Image src="/apple" alt="Apple" className="w-[24px] h-[24px] left-[16px] mr-2" />
               Sign up with Apple
             </button>
           </div>
